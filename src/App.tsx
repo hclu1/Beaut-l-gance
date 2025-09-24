b6458c70556a333b12ad29e22208a27a1ba5af6e
herve <hchampag1@hotmail.fr>
2025-09-24 13:17:11 +0200

    Amélioration responsive + QR Code + correction champs admin

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import ProductCard from './components/ProductCard';
 import Cart from './components/Cart';
 import FloatingCartIcon from './components/FloatingCartIcon';
 import CheckoutModal from './components/CheckoutModal';
-import AdminPanel from './components/AdminPanel.tsx.old';
+import AdminPanel from './components/AdminPanel';
 import { ProductService } from './services/productService';
 
 const App: React.FC = () => {
